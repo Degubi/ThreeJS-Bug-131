b443fae4875e1019ce6d0842011f72abf32c9abf
Degubi <gergonoorbi@gmail.com>
2021-08-17 23:12:38 +0200

    Make code even simpler

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const workDir = resolve();
 const server = express();
 
 server.use(express.static(`${workDir}`));
-server.get('/', (_, res) => res.sendFile(`${workDir}/viewer.html`));
+server.get('/', (_, res) => res.sendFile(`${workDir}/index.html`));
 server.listen(process.env.PORT ?? 8080);
